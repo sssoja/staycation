d86d5a4ec5b2477e4158491d67c5d00bd19c59e4
sophie <sophieleila@icloud.com>
2020-04-08 20:07:33 +0200

    implement CRUD operations on listing and user files

--- routes/index.js
+++ routes/index.js
@@ -4,18 +4,23 @@ const bodyParser = require("body-parser");
 
 router.use(bodyParser.json());
 
-// HTTP Methods
-const userFunctions = require("./users.js");
-const listingFunctions = require("./listings.js");
-
 router.get("/", (req, res, next) => {
   res.send("Welcome to the staycation API");
 });
 
-//router.get("/users", userFunctions.getUsers);
-router.post("/users", userFunctions.createUser);
+// HTTP Methods
+const userFunctions = require("./users.js");
+const listingFunctions = require("./listings.js");
 
+router.get("/users", userFunctions.getUsers);
+router.get("/users/:user_id", userFunctions.getUserById);
+router.post("/users", userFunctions.createUser);
+router.put("/users/:user_id", userFunctions.updateUser);
+router.delete("/users/:user_id", userFunctions.deleteUser);
 router.get("/listings", listingFunctions.getListings);
+router.get("/listings/:listing_id", listingFunctions.getListingById);
 router.post("/listings", listingFunctions.createListing);
+router.put("/listings/:listing_id", listingFunctions.updateListing);
+router.delete("/listings/:listing_id", listingFunctions.deleteListing);
 
 module.exports = router;
