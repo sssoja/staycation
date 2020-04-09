133021e4c9a4256c9328336b22cd3288a9a6f992
Mimi <mimi@MacBook-Air-Oj-11.local>
2020-04-09 18:44:21 +0200

    CREATING

--- app.js
+++ app.js
@@ -4,8 +4,10 @@ var path = require("path");
 var cookieParser = require("cookie-parser");
 var logger = require("morgan");
 
-var indexRouter = require("./routes/index");
-//var usersRouter = require("./routes/users");
+//var indexRouter = require("./routes/index");
+var usersRouter = require("./routes/users");
+var listingsRouter = require("./routes/listings");
+// router.use(bodyParser.json());
 
 var app = express();
 
@@ -15,23 +17,30 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, "public")));
 
-app.use("/", indexRouter);
+app.get("/", function (req, res, next) {
+    res.send("Welcome to staycation!");
+});
+//app.use("/", indexRouter);
 //app.use("/users", usersRouter);
+
+// //ADDED
+app.use("/staycation", usersRouter);
+//app.use("/staycation", listingsRouter);
 
 // catch 404 and forward to error handler
 app.use(function (req, res, next) {
-  next(createError(404));
+    next(createError(404));
 });
 
 // error handler
 app.use(function (err, req, res, next) {
-  // set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = req.app.get("env") === "development" ? err : {};
+    // set locals, only providing error in development
+    res.locals.message = err.message;
+    res.locals.error = req.app.get("env") === "development" ? err : {};
 
-  // render the error page
-  res.status(err.status || 500);
-  res.send("error");
+    // render the error page
+    res.status(err.status || 500);
+    res.send("error");
 });
 
 module.exports = app;
--- routes/users.js
+++ routes/users.js
@@ -1,55 +1,73 @@
+// var express = require("express");
+// const db = require("../model/helper");
+
 var express = require("express");
+var router = express.Router();
+const bodyParser = require("body-parser");
 const db = require("../model/helper");
 
-function getUsers(req, res, next) {
-  db(`SELECT * FROM users ORDER BY id ASC;`)
-    .then((results) => {
-      res.send(results.data);
-    })
-    .catch((err) => res.status(500).send(err));
-}
+router.use(bodyParser.json());
 
-function createUser(req, res) {
-  db(`INSERT INTO users (email, phone_number, num_country_code, name, preferred_contact_method) VALUES 
-  ("${req.body.email}","${req.body.phone_number}","${req.body.num_country_code}","${req.body.name}","${req.body.preferred_contact_method}");`)
-    .then((results) => {
-      getUsers(req, res, next);
-    })
-    .catch((err) => res.status(500).send(err));
-}
+const VERSION = "v1";
 
-function getUserById(req, res) {
-  db(`SELECT * FROM users WHERE id=${req.params.user_id};`)
-    .then((results) => {
-      getUsers(req, res, next);
-    })
-    .catch((err) => res.status(500).send(err));
-}
+router.get('/users', (req, res) => {
+    // Respond by send the full list of data in "users" table
+    db("SELECT * FROM users ORDER BY id ASC;")
+        .then(results => {
+            res.send(results.data);
+        })
+        .catch(err => res.status(500).send(err));
+});
 
-function updateUser(req, res) {
-  db(
-    `UPDATE users SET email=${req.body.email}, phone_number=${req.body.phone_number}, 
-    num_country_code=${req.body.num_country_code}, name=${req.body.name}, 
-    preferred_contact_method=${req.body.preferred_contact_method} WHERE id=${req.params.user_id};`
-  )
-    .then((results) => {
-      getUsers(req, res, next);
-    })
-    .catch((err) => res.status(500).send(err));
-}
 
-function deleteUser(req, res) {
-  db(`DELETE FROM users WHERE id=${req.params.user_id};`)
-    .then((results) => {
-      getUsers(req, res, next);
-    })
-    .catch((err) => res.status(500).send(err));
-}
 
-module.exports = {
-  getUsers,
-  createUser,
-  getUserById,
-  updateUser,
-  deleteUser,
-};
+
+
+
+// function createUser(req, res) {
+//     db(`INSERT INTO users (email, phone_number, num_country_code, name, preferred_contact_method) VALUES
+//   ("${req.body.email}","${req.body.phone_number}","${req.body.num_country_code}","${req.body.name}","${req.body.preferred_contact_method}");`)
+//         .then((results) => {
+//             getUsers(req, res, next);
+//         })
+//         .catch((err) => res.status(500).send(err));
+// }
+
+// function getUserById(req, res) {
+//     db(`SELECT * FROM users WHERE id=${req.params.user_id};`)
+//         .then((results) => {
+//             getUsers(req, res, next);
+//         })
+//         .catch((err) => res.status(500).send(err));
+// }
+
+// function updateUser(req, res) {
+//     db(
+//             `UPDATE users SET email=${req.body.email}, phone_number=${req.body.phone_number},
+//     num_country_code=${req.body.num_country_code}, name=${req.body.name},
+//     preferred_contact_method=${req.body.preferred_contact_method} WHERE id=${req.params.user_id};`
+//         )
+//         .then((results) => {
+//             getUsers(req, res, next);
+//         })
+//         .catch((err) => res.status(500).send(err));
+// }
+
+// function deleteUser(req, res) {
+//     db(`DELETE FROM users WHERE id=${req.params.user_id};`)
+//         .then((results) => {
+//             getUsers(req, res, next);
+//         })
+//         .catch((err) => res.status(500).send(err));
+// }
+
+// module.exports = {
+//     getUsers,
+//     createUser,
+//     getUserById,
+//     updateUser,
+//     deleteUser,
+// };
+
+
+module.exports = router;
