a0e2ff3e45eae8fb95e9584407364c209c6e9230
Mimi <mimi@MacBook-Air-Oj-11.local>
2020-04-09 11:39:06 +0200

    setting up

--- model/database.js
+++ model/database.js
@@ -7,23 +7,23 @@ const DB_PASS = process.env.DB_PASS;
 const DB_NAME = process.env.DB_NAME;
 
 const con = mysql.createConnection({
-  host: DB_HOST || "127.0.0.1",
-  user: DB_USER || "root",
-  password: DB_PASS,
-  database: DB_NAME || "staycation",
-  multipleStatements: true,
+    host: DB_HOST || "127.0.0.1",
+    user: DB_USER || "root",
+    password: DB_PASS || "root",
+    database: DB_NAME || "staycation",
+    multipleStatements: true,
 });
 
-con.connect(function (err) {
-  if (err) throw err;
-  console.log("Connected!");
+con.connect(function(err) {
+    if (err) throw err;
+    console.log("Connected!");
 
-  //edit this to create database tables
-  let sql =
+    //edit this to create database tables
+    let sql = "";
     // Here we are making sure to wipe out any tables that already exist!
     "DROP TABLE if exists users; DROP TABLE if exists listings; DROP TABLE if exists locations; DROP TABLE if exists bookings; DROP TABLE if exists reviews; " +
     // Here we actually create the tables
-    "CREATE TABLE users ( id integer PRIMARY KEY, email nvarchar(255) UNIQUE NOT NULL, phone_number integer UNIQUE, num_country_code integer, name nvarchar(255), preferred_contact_method nvarchar(255) ); " +
+    "CREATE TABLE users ( id integer PRIMARY KEY, email nvarchar(255) UNIQUE NOT NULL, phone_number integer UNIQUE, num_country_code integer, name nvarchar(255), preferred_contact_method varchar(255) ); " +
     "CREATE TABLE listings ( id integer PRIMARY KEY, user_id integer, date_published date, space_type nvarchar(255), is_shared boolean, location_id integer, reviews_id integer ); " +
     "CREATE TABLE locations ( id integer PRIMARY KEY, street_name nvarchar(255), city_name nvarchar(255), latitude float, longitude float ); " +
     "CREATE TABLE bookings ( id integer PRIMARY KEY, user_id integer, listing_id integer, start_date date, end_date date ); " +
@@ -35,14 +35,14 @@ con.connect(function (err) {
     "ALTER TABLE bookings ADD FOREIGN KEY (listing_id) REFERENCES listings (id); " +
     "ALTER TABLE reviews ADD FOREIGN KEY (booking_id) REFERENCES bookings (id); ";
 
-  con.query(sql, function (err, result) {
-    if (err) throw err;
-    console.log(
-      "Table creation: `users`, `listings`, `locations`, `bookings` was successful!"
-    );
+    con.query(sql, function(err, result) {
+        if (err) throw err;
+        console.log(
+            "Table creation: `users`, `listings`, `locations`, `bookings` was successful!"
+        );
 
-    console.log("Closing...");
-  });
+        console.log("Closing...");
+    });
 
-  con.end();
+    con.end();
 });
